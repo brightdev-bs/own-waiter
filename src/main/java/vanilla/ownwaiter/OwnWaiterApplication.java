19ce9eead406d6bc56d81115c1d5007fb843376f
vanilla <kr.vanille@gmail.com>
2022-07-10 23:05:58 +0900

    음식 목록, 주문 관리 페이지 컨트롤러 연결.
    레스토랑, 유저 m:n관계 -> 데이터베이스 설계 해야 함.

--- src/main/java/vanilla/ownwaiter/OwnWaiterApplication.java
+++ src/main/java/vanilla/ownwaiter/OwnWaiterApplication.java
@@ -3,22 +3,34 @@ package vanilla.ownwaiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import vanilla.ownwaiter.entity.Restaurant;
+import vanilla.ownwaiter.entity.user.User;
 import vanilla.ownwaiter.repository.RestaurantRepository;
+import vanilla.ownwaiter.service.UserService;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SpringBootApplication
 @RequiredArgsConstructor
 public class OwnWaiterApplication {
 
 	private final RestaurantRepository restaurantRepository;
+	private final UserService userService;
 
 	@PostConstruct
 	void init() {
 		Restaurant res1 = Restaurant.builder().name("김밥천국1").location("도곡점").build();
 		Restaurant res2 = Restaurant.builder().name("김밥천국2").location("매봉점").build();
 		restaurantRepository.save(res1); restaurantRepository.save(res2);
+
+		User admin = User.builder().email("manager@gmail.com").password("123").authorities(new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority("ADMIN")))).build();
+		User user = User.builder().email("user@gmail.com").password("123").authorities(new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority("USER")))).build();
+		userService.save(admin);
+		userService.save(user);
 	}
 
 	public static void main(String[] args) {
