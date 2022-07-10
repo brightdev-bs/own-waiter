1b5892df56d5f6ec6d97d04fc0e8788f3f6b4a40
Byeongsu KIM <brightdev.bs@gmail.com>
2022-07-10 23:05:58 +0900

    음식 목록, 주문 관리 페이지 컨트롤러 연결.
    레스토랑, 유저 m:n관계 -> 데이터베이스 설계 해야 함.

--- src/main/java/vanilla/ownwaiter/controller/ManagerController.java
+++ src/main/java/vanilla/ownwaiter/controller/ManagerController.java
@@ -1,15 +1,42 @@
 package vanilla.ownwaiter.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import vanilla.ownwaiter.entity.Restaurant;
+import vanilla.ownwaiter.entity.user.User;
+import vanilla.ownwaiter.service.RestaurantService;
 
 @Controller
 @RequestMapping("/manager")
+@RequiredArgsConstructor
 public class ManagerController {
 
-//    @GetMapping
-//    public String loadMainPage() {
-//
-//    }
+    private final RestaurantService restaurantService;
+
+    @GetMapping
+    public String loadMainPage() {
+        return "manager/homeManager";
+    }
+
+    @GetMapping("/manager/foodList")
+    public String moveToFoodManage(Authentication auth, Model model) {
+        User manager = (User) auth.getDetails();
+        Restaurant findRes = restaurantService.findById(manager.getId());
+        model.addAttribute("items", findRes.getItems());
+        return "/manager/foodList";
+    }
+
+    @GetMapping("/manager/orderList")
+    public String moveToOrderList(Authentication auth, Model model) {
+        User manager = (User) auth.getDetails();
+        Restaurant findRes = restaurantService.findById(manager.getId());
+//        model.addAttribute("items", findRes.getItems());
+        return "/manager/orerList";
+    }
+
+
 }
