74254fda8029b9c55691c28f6300fe4e16199fba
vanilla <kr.vanille@gmail.com>
2022-07-11 17:01:34 +0900

    jpa auto @CreateDate and @LastModifiedDate 추가.

--- src/main/java/vanilla/ownwaiter/OwnWaiterApplication.java
+++ src/main/java/vanilla/ownwaiter/OwnWaiterApplication.java
@@ -3,6 +3,7 @@ package vanilla.ownwaiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import vanilla.ownwaiter.entity.Restaurant;
@@ -14,6 +15,7 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @RequiredArgsConstructor
 public class OwnWaiterApplication {
@@ -27,8 +29,8 @@ public class OwnWaiterApplication {
 		Restaurant res2 = Restaurant.builder().name("김밥천국2").location("매봉점").build();
 		restaurantRepository.save(res1); restaurantRepository.save(res2);
 
-		User admin = User.builder().email("manager@gmail.com").password("123").authorities(new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority("ADMIN")))).build();
-		User user = User.builder().email("user@gmail.com").password("123").authorities(new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority("USER")))).build();
+		User admin = User.builder().username("kim").email("manager@gmail.com").password("123").authorities(new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority("ADMIN")))).build();
+		User user = User.builder().username("lee").email("user@gmail.com").password("123").authorities(new ArrayList<GrantedAuthority>(Arrays.asList(new SimpleGrantedAuthority("USER")))).build();
 		userService.save(admin);
 		userService.save(user);
 	}
