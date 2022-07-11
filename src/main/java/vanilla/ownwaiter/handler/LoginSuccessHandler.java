e04d5d2ef1f47aa11894e5ce11a6835124ea896c
Byeongsu KIM <brightdev.bs@gmail.com>
2022-07-11 14:29:04 +0900

    loginHandler 작성

---
+++ src/main/java/vanilla/ownwaiter/handler/LoginSuccessHandler.java
@@ -0,0 +1,31 @@
+package vanilla.ownwaiter.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import vanilla.ownwaiter.entity.user.User;
+import vanilla.ownwaiter.entity.user.UserRole;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        User user = (User) authentication.getPrincipal();
+        UserRole role = user.getRole();
+        if(role == UserRole.USER) {
+            response.sendRedirect("/home");
+        }
+
+        if(role == UserRole.ADMIN) {
+            response.sendRedirect("/manager");
+        }
+    }
+}
