86c66918c319a0b4c74da5b7a307ba95d5649ed4
Byeongsu KIM <brightdev.bs@gmail.com>
2022-07-12 15:27:07 +0900

    음식 목록 관리 기능 구현

--- src/main/java/vanilla/ownwaiter/controller/ManagerController.java
+++ src/main/java/vanilla/ownwaiter/controller/ManagerController.java
@@ -1,42 +1,79 @@
 package vanilla.ownwaiter.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vanilla.ownwaiter.entity.Restaurant;
+import vanilla.ownwaiter.entity.dto.RestaurantForm;
 import vanilla.ownwaiter.entity.user.User;
 import vanilla.ownwaiter.service.RestaurantService;
+import vanilla.ownwaiter.service.UserService;
 
+import java.io.IOException;
+
+@Slf4j
 @Controller
 @RequestMapping("/manager")
 @RequiredArgsConstructor
 public class ManagerController {
 
     private final RestaurantService restaurantService;
+    private final UserService userService;
 
     @GetMapping
     public String loadMainPage() {
+
         return "manager/homeManager";
     }
 
     @GetMapping("/foodList")
-    public String moveToFoodManage(Authentication auth, Model model) {
-        User manager = (User) auth.getDetails();
-        Restaurant findRes = restaurantService.findById(manager.getId());
-        model.addAttribute("items", findRes.getItems());
+    public String moveToFoodList(Authentication auth, Model model) {
+        User manager = getUser(auth);
+        model.addAttribute("foods", manager.getRestaurant().getFoods());
         return "/manager/foodList";
     }
 
     @GetMapping("orderList")
     public String moveToOrderList(Authentication auth, Model model) {
-        User manager = (User) auth.getDetails();
-        Restaurant findRes = restaurantService.findById(manager.getId());
-//        model.addAttribute("items", findRes.getItems());
-        return "/manager/orerList";
+        User manager = getUser(auth);
+        Restaurant restaurant = manager.getRestaurant();
+        model.addAttribute("restaurant", restaurant);
+        return "/manager/orderList";
     }
+
+    @GetMapping("/register/restaurant")
+    public String registerRestaurant(Model model) {
+        model.addAttribute("restaurantForm", new RestaurantForm());
+        return "/manager/registerRestaurant";
+    }
+
+    @PostMapping("/register/restaurant")
+    public String registerRestaurant(@ModelAttribute RestaurantForm restaurantForm, Authentication auth) throws IOException {
+        Restaurant res = restaurantService.uploadProfileAndSetImg(restaurantForm.toEntity(), restaurantForm.getImg());
+
+        User user = getUser(auth);
+        user.registerRestaurant(res);
+        userService.save(user);
+
+        return "redirect:/manager";
+    }
+
+    @GetMapping("/register/food")
+    public String moveToRegisterFood() {
+        return "/manager/registerFood";
+    }
+
+    private User getUser(Authentication auth) {
+        User user = (User) auth.getPrincipal();
+        return user;
+    }
+
 
 
 }
--- src/main/java/vanilla/ownwaiter/service/RestaurantService.java
+++ src/main/java/vanilla/ownwaiter/service/RestaurantService.java
@@ -2,26 +2,38 @@ package vanilla.ownwaiter.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import vanilla.ownwaiter.entity.Restaurant;
+import vanilla.ownwaiter.file.S3Uploader;
 import vanilla.ownwaiter.repository.RestaurantRepository;
 
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
+    private final S3Uploader s3Uploader;
 
+    @Transactional
     public Restaurant save(Restaurant restaurant) {
         // location이랑 이름을 합쳐서 저장하게 끔 하는게 좋을 것 같음.
         // bc 김밥천국 도곡점, 김밥천국 매봉점, 이런식으로 하기 위함.
         return restaurantRepository.save(restaurant);
+    }
+
+    @Transactional
+    public Restaurant uploadProfileAndSetImg(Restaurant restaurant, MultipartFile img) throws IOException {
+        String uploadUrl = s3Uploader.upload(img, "restaurant");
+        restaurant.setProfileImg(uploadUrl);
+        restaurantRepository.save(restaurant);
+        return restaurant;
     }
 
     public Restaurant findById(Long id) {
