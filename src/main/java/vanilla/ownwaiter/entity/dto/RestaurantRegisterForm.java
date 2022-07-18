ef3a0cb905d5518d3afb529320d6ef4f276f11b4
vanilla <kr.vanille@gmail.com>
2022-07-18 16:37:10 +0900

    레스토랑 이미지 업로드 리팩토링

--- src/main/java/vanilla/ownwaiter/entity/dto/RestaurantRegisterForm.java
+++ src/main/java/vanilla/ownwaiter/entity/dto/RestaurantRegisterForm.java
@@ -1,8 +1,16 @@
 package vanilla.ownwaiter.entity.dto;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import vanilla.ownwaiter.entity.Restaurant;
+import vanilla.ownwaiter.file.S3Uploader;
+
+import javax.persistence.Entity;
+import java.io.IOException;
 
 @Data
 public class RestaurantRegisterForm {
@@ -11,6 +19,9 @@ public class RestaurantRegisterForm {
     private String location;
     private String description;
     private MultipartFile img;
+
+    public RestaurantRegisterForm() {
+    }
 
     public Restaurant toEntity() {
         return Restaurant.builder()
