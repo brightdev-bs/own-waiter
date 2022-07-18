27a1531b3d6688a14eceb86346fb6c3f436e28bd
vanilla <kr.vanille@gmail.com>
2022-07-18 15:44:21 +0900

    로그인 실패 핸들러 등록

---
+++ src/main/java/vanilla/ownwaiter/handler/LoginFailureHandler.java
@@ -0,0 +1,42 @@
+package vanilla.ownwaiter.handler;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String errorMessage;
+        if(exception instanceof BadCredentialsException) {
+            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다.";
+        } else if (exception instanceof InternalAuthenticationServiceException) {
+            errorMessage = "내부적으로 발생한 시스템 문제로 인해 요청을 처리할 수 없습ㄴ시다.";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "계정이 존재하지 않습니다.";
+        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
+            errorMessage = "인증 요청이 거부되었습니다. ";
+        } else {
+            errorMessage = "알수 없는 이유로 로그인에 실패하였습니다.";
+        }
+
+        System.out.println(errorMessage);
+
+        setDefaultFailureUrl("/login?error=true&exception=" + errorMessage);
+
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
+
